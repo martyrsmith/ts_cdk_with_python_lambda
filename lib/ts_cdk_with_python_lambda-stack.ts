877371753e8ad0610c16de6158fc48b749be67b4
Martin Smith <39108172+martyrsmith@users.noreply.github.com>
2022-06-22 18:05:25 -0500

    python lambda

--- lib/ts_cdk_with_python_lambda-stack.ts
+++ lib/ts_cdk_with_python_lambda-stack.ts
@@ -1,16 +1,23 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { PythonFunction } from "@aws-cdk/aws-lambda-python-alpha";
+import { aws_apigateway as apigw, aws_lambda as lambda } from "aws-cdk-lib";
+
 
 export class TsCdkWithPythonLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const myPython = new PythonFunction(this, 'MyPythonHandler', {
+      entry: './lambda_python',  // Folder containing lambda
+      runtime: lambda.Runtime.PYTHON_3_9,  // Python version
+      index: 'main.py',  // Name of Python file
+      handler: 'handler'  // Name of method
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TsCdkWithPythonLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // API Gateway Python2
+    new apigw.LambdaRestApi(this, 'MyPythonEndpoint', {
+      handler: myPython
+    })
   }
 }
